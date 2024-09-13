8064317141a0fe600ca5f5df27a6a270ba7effde
baoxiaodong <lonseajim@163.com>
2024-09-13 19:05:36 +0800

    update README.md

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# Siamese-Network
-Siamese Network for Classification of Raman Spectroscopy with Inter-instrument Variation for Biological Applications
+# Siamese-Network for Raman spectra
+
+'Siamese Network for Classification of Raman Spectroscopy with Inter-instrument Variation for Biological Applications'
+
+## Creating Dataset
+
+### Samples: 10 bacteria
+
+* Acinetobacter baumannii (ABA)
+* Citrobacter freundii (CFR)
+* Escherichia coli (ECO)
+* Enterococcus faecalis (EFA)
+* Klebsiella oxytoca (KOX)
+* Klebsiella pneumoniae (KPN)
+* Stenotrophomonas maltophilia (PMA)
+* Staphylococcus aureus (SAU)
+* Staphylococcus hominis (SHO)
+* Serratia marcescens (SMA)
+
+### Raman spectrometer
+
+* Hooke P300 (Hooke Instruments, Changchun, China.) 600g/mm grating and 1200g/mm grating
+* Hooke R300 (Hooke Instruments, Changchun, China.) 600g/mm grating
+
+### Raman spectra acquisition conditions
+
+* Excitation wavelength: 532nm
+* Power: 5mW
+* Exposure time: 5s
+* Number: 400 spectra for each bacterial with each instrument model
+
+## Data preprocessing
+
+The preprocessing function is in **'precess_fun.py'**.
+
+Load data by the class in **'base.py'** and set **process_flag=True**. The steps of data preprocessing are as follows:
+
+* Step 1：spike removal
+* Step 2：baseline subtraction
+* Step 3：normalization
+
+Then, the preprocessed data can be saved in '.txt' or '.npy' files.
+
+## Data preparation
+
+There 3 functions in **'dataset.py'** to create csv files for data preparation.
+
+* **'create_folder_csv'**: Create a csv file with data folder.
+* **'create_fsl_ds_csv'**: Split two csv files into four csv files for training and testing.
+* **'merge_fsl_csv'**: Merge two csv files into one with positive and negative pairs for SNN model Training
+
+## Model training
+
+### Classification task
+
+> notebook/train_classification.ipynb
--- dataset.py
+++ dataset.py
@@ -1,7 +1,12 @@
-from torch.utils.data import Dataset
-import numpy as np
 import random
+
+import numpy as np
+import pandas as pd
 import torch
+from sklearn.model_selection import StratifiedKFold
+from torch.utils.data import Dataset
+
+from base import SpectraFolder
 
 
 class CFDataset(Dataset):
@@ -64,3 +69,75 @@ def snn_batch(df, batch_size: int = 512, pos_neg_ratio=0.5):
     sample2_list = np.expand_dims(sample2_list, 1)
 
     return torch.tensor(sample1_list), torch.tensor(sample2_list), torch.tensor(label_list)
+
+
+def create_folder_csv(folder_src, csv_path=None):
+    """
+    create a csv file with data folder
+    """
+    sf = SpectraFolder(folder_src)
+    df = sf.get_df()
+    if csv_path is not None:
+        df.to_csv(csv_path)
+    else:
+        df.to_csv('dataset.csv')
+
+
+def create_fsl_ds_csv(query_csv, support_csv, n_splits=5, n_export=5):
+    """
+    create csv files for few-shot learning training and testing
+    """
+    assert 0 < n_export <= n_splits
+
+    query_df = pd.read_csv(query_csv, index_col=0)
+    support_df = pd.read_csv(support_csv, index_col=0)
+
+    query_file_list = query_df['data_file'].to_numpy()
+    query_label_list = query_df['label'].to_numpy()
+
+    skf = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=42)
+
+    for idx, (train_index, test_index) in enumerate(skf.split(query_file_list, query_label_list)):
+        if idx >= n_export:
+            break
+
+        train_query_df = query_df.iloc[test_index]
+        train_support_df = support_df.iloc[test_index]
+
+        test_query_df = query_df.iloc[train_index]
+        test_support_df = support_df.iloc[train_index]
+
+        few_query_len = len(test_index)
+        test_query_len = len(train_index)
+
+        # query dataset
+        train_query_df.to_csv('train_P300_600_cv{}_{}.csv'.format(idx + 1, few_query_len))
+        # support dataset
+        train_support_df.to_csv('train_R300_1200_cv{}_{}.csv'.format(idx + 1, few_query_len))
+
+        # for model testing
+        test_query_df.to_csv('test_P300_600_cv{}_{}.csv'.format(idx + 1, test_query_len))
+        test_support_df.to_csv('test_R300_1200_cv{}_{}.csv'.format(idx + 1, test_query_len))
+
+
+def merge_fsl_csv(cv=1):
+    """
+    merge two csv files into one csv file for snn model training
+    create pos and neg pairs for dataset
+    """
+    p300_df = pd.read_csv('train_P300_600_cv{}_100.csv'.format(cv))
+    r300_df = pd.read_csv('train_R300_1200_cv{}_100.csv'.format(cv))
+    data_list = []
+    for p300_i, p300_row in p300_df.iterrows():
+        p300_data_file = p300_row['data_file']
+        p300_data_label = p300_row['label']
+        for r300_i, r300_row in r300_df.iterrows():
+            r300_data_file = r300_row['data_file']
+            r300_data_label = r300_row['label']
+            if p300_data_label == r300_data_label:
+                label = 1
+            else:
+                label = 0
+            data_list.append([p300_data_file, r300_data_file, label])
+    df = pd.DataFrame(data_list, columns=['p300_file', 'r300_file', 'label'])
+    df.to_csv('fsl2_cv{}_{}.csv'.format(cv, len(data_list)))
